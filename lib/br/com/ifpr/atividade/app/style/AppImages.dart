b8fcf3c3eb163aae4fc12da63c4616226885c9a1
flag-fix <isac.bandeir4@gmail.com>
2022-07-13 20:06:24 -0300

    Envio do main

---
+++ lib/br/com/ifpr/atividade/app/style/AppImages.dart
@@ -0,0 +1,7 @@
+class AppImages {
+  static String get zoo => "assets/img/zoo.png";
+  static String get animal => "assets/img/animal.png";
+  static String get sistemaZoo => "assets/img/sistemaZoo.png";
+  static String get addAnimal => "assets/img/addAnimal.png";
+  static String get listaAnimais => "assets/img/listaAnimais.png";
+}
